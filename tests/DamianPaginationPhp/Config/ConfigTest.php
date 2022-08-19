beaf2647c1fe67b516cfbae812455982ce4c52fd
s-damian <contact@devandweb.fr>
2022-08-19 14:39:20 +0300

    Optimize phpstan to level 7

--- tests/DamianPaginationPhp/Config/ConfigTest.php
+++ tests/DamianPaginationPhp/Config/ConfigTest.php
@@ -9,14 +9,14 @@ class ConfigTest extends BaseTest
 {
     public function testConfig(): void
     {
-        $this->assertSame('en', Config::get()['lang']);
+        $this->assertSame('en', Config::get()['lang'] ?? ''); // "?? ''" pour phpstan (level 7)
 
         $this->assertTrue(is_array(Config::get()));
 
         // Et on change la langue.
         Config::set(['lang' => 'fr']);
 
-        $this->assertSame('fr', Config::get()['lang']);
+        $this->assertSame('fr', Config::get()['lang'] ?? ''); // "?? ''" pour phpstan (level 7)
 
         $this->assertTrue(is_array(Config::get()));
     }
